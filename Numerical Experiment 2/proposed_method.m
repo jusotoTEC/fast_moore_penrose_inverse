1468056d425abc87ee29ac3483c555307ffb4e86
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2024-07-10 11:20:31 -0600

    Update proposed_method.m

--- Numerical Experiment 2/proposed_method.m
+++ Numerical Experiment 2/proposed_method.m
@@ -5,7 +5,7 @@ function X=proposed_method(A,s,tol)
 %                      Positive singular values s of size r             
 %                      Constante tol > 0
 %              Output: Pseudoinverse X of size n x m
-% Reference: Soto-Quiros, P. (2022), A fast method to estimate the Moore-Penrose 
+% Reference: Soto-Quiros, P. (2024), A fast method to estimate the Moore-Penrose 
 % inverse for well-determined numerical rank matrices based on the Tikhonov 
 % regularization. (Submitted paper)
 
