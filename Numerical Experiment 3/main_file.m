1a5e1c10eff78d36aba0e16e66e9fe75d08daa2b
jusotoTEC <102713314+jusotoTEC@users.noreply.github.com>
2024-09-13 04:34:38 -0600

    Update main_file.m

--- Numerical Experiment 3/main_file.m
+++ Numerical Experiment 3/main_file.m
@@ -4,7 +4,7 @@ function main_file()
 
     % Reference: Soto-Quiros, P. (2024), A fast method to estimate the Moore-Penrose 
     %            inverse for well-determined numerical rank matrices based on the 
-    %            Tikhonov regularization. (Submitted paper)
+    %            Tikhonov regularization. 
 
     clc; clear;  close all   
     numImg=1110; %Number of images in folders "image_free_noise" and "image_with_noise"
